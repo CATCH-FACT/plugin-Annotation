fb34beb93a82cbbfaad82a9932417dbccf294f18
Iwe Muiser <e.c.muiser@gmail.com>
2016-04-04 11:30:44 +0200

    redesigning clone 3

--- views/admin/duplicate/index.php
+++ views/admin/duplicate/index.php
@@ -1,13 +1,1 @@
-<?php 
-    echo head(array('title' => 'Duplicate Item', 'bodyclass' => 'primary', 
-        'content_class' => 'horizontal-nav'));
-
-    echo $this->partial('Annotation-navigation.php');
-?>
-
-<div id="primary">
-</div>
-
-<?php 
-    echo foot(); 
-?>
+test
