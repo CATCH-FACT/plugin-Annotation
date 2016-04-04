d4d55fdff987efbc5d659f6cb8e56768e3d2eaef
Iwe Muiser <e.c.muiser@gmail.com>
2016-04-04 11:23:48 +0200

    redesigning clone 3

--- views/admin/duplicate/index.php
+++ views/admin/duplicate/index.php
@@ -1,1 +1,19 @@
-test
+<?php 
+    echo head(array('title' => 'Duplicate Item', 'bodyclass' => 'primary', 
+        'content_class' => 'horizontal-nav'));
+
+    echo $this->partial('Annotation-navigation.php');
+?>
+
+<div id="primary">
+    <?php echo flash(); ?>
+    <h2><?php echo __('You want to duplicate the following item:'); ?></h2>
+    
+    <?php echo "Original item: " . link_to_item(metadata($record, array('Dublin Core', 'Identifier')), array(), 'show', $record); ?>
+    <br>
+    <br>
+    <?php echo $this->form; ?>
+</div>
+<?php 
+    echo foot(); 
+?>
